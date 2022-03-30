584744c97740e94fa813dab093abc8b47c5f9eea
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-30 17:20:02 +0200

    Importing Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-danger">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quo id obcaecati odio veniam sit quis ducimus aliquam similique natus, fugiat architecto odit dolorem a maiores ea sed? Quis, quasi?</h1>
+    <AlbumsList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlbumsList from './components/AlbumsList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AlbumsList,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/style/main-style.scss';
+
+
 </style>
---
+++ src/components/AlbumsList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AlbumsList'
+}
+</script>
+
+<style>
+
+</style>
